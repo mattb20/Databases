2a31c2874f5ab045908b5dcf01d340d8b488c707
Matthew <matt_burgoyne@live.co.uk>
2018-04-09 12:55:23 +0100

    gemfile, spec_helper created

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  ENV['RACK_ENV'] = 'test'
+  require './app.rb'
+  require 'capybara'
+  require 'capybara/rspec'
+  Capybara.app = BookmarkApp
+end
